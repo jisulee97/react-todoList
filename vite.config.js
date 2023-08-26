4c3b0309b4da7bd519e02e4a24bc27f8662a5b4e
leejisu <ejeesoo97@gmail.com>
2023-08-26 14:08:14 +0900

    Add : 배포시 base 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import jsConfigPath from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-todoList/",
   plugins: [react(), jsConfigPath()],
 });
